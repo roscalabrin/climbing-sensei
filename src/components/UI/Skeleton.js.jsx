9c887ca464b99787be50fcf1e206b63314871f05
pwentz <patrick.w.wentz@gmail.com>
2016-10-25 10:03:13 -0600

    close #9 add carousel and placeholders for skeleton UI component.

--- src/components/UI/Skeleton.js.jsx
+++ src/components/UI/Skeleton.js.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
-import { Col } from 'react-bootstrap'
+import { Col, Carousel} from 'react-bootstrap'
 import './styles/Skeleton.css'
 
 class Skeleton extends Component {
   render() {
+    const placeHolder = <img width={350} height={150} src='https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150'></img>
     return (
       <Col className='skeleton-container' sm={8} md={4}>
-        {this.props.skellys}
+        <Carousel indicators={false} interval={0}>
+          <Carousel.Item>
+            { placeHolder }
+          </Carousel.Item>
+          <Carousel.Item>
+            { placeHolder }
+          </Carousel.Item>
+        </Carousel>
       </Col>
     )
   }
