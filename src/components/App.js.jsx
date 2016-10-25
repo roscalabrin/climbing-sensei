60e9f8e4f27df160036d1008910a48b99c320352
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-25 10:15:21 -0600

    add react router and links to navbar

--- src/components/App.js.jsx
+++ src/components/App.js.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 import '../css/App.css'
 import BuilderContainer from './containers/BuilderContainer.js.jsx'
 import DashboardContainer from './containers/DashboardContainer.js.jsx'
@@ -7,8 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        // <BuilderContainer />
-        <DashboardContainer />
+        <p><Link to="/my-account">My Account</Link></p>
+        { this.props.children }
       </div>
     )
   }
